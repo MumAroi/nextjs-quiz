59b284d804ebafa4ebec8656f39534848078a5ec
Paramas <paramas.w@livinginsider.com>
2023-09-02 09:43:17 +0700

    feat: ✨ add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+// export { default } from "next-auth/middleware";
+import withAuth from "next-auth/middleware";
+
+export default withAuth({
+	pages: { signIn: "/" },
+});
+
+export const config = { matcher: ["/dashboard", "/quiz"] };
